7777da8ec345e8d1ed69caae94aa754e947e3288
pranalipatil209 <pranalipatil209@gmail.com>
2017-03-25 19:17:02 +0530

    unmarked employee cards displayed

--- controller/UnmarkedEmpctrl.js
+++ controller/UnmarkedEmpctrl.js
@@ -3,12 +3,13 @@
  bind the controller with the module and inject the services
  */
 angular.module('mainApp').controller('unmarkedEmp', function ($scope, $stateParams, $state, $http, localStorageService, restService) {
-    console.log('hieee');
     var query = { timeStamp : $stateParams.timeStamp};
     console.log(query);
-    restService.httpRequest('readUnmarkedAttendanceEmployee',
-        query, "get").then(function (data) {
-            console.log('res ',data);
+    restService.httpRequest('readUnmarkedAttendanceEmployee', query, "get").then(function (data) {
+        console.log('res ',data);
+        var date = new Date(parseInt(data.data.timeStamp));
+        var locale = "en-us";
+        $scope.date = date.getDate() +' '+ (date.toLocaleString(locale, { month: "long" })) +', '+ date.getFullYear();
         $scope.totalEmployee = data.data.totalEmployee;
         $scope.unmarkedNumber = data.data.unmarkedNumber;
         $scope.unmarkedData = data.data.umarkedEmployee;
