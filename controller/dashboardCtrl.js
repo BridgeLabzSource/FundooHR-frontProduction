d5839bdd3f6eca5d001c90155dd0343227d0a5e9
Pranali-Patil <pranali.patil@bridgelabz.com>
2017-05-10 18:43:03 +0530

    dashboard updated with working new add employee form

--- controller/dashboardCtrl.js
+++ controller/dashboardCtrl.js
@@ -4,27 +4,25 @@
  *@param {string} dashCtrl - parameter refers to the controller used by HTML element
  *@param {function} selfInvoked- dependencies are added in it
  */
-angular.module('mainApp').controller('dashboardCtrl', function ($scope,restService,$location, ngDialog) {
+angular.module('mainApp').controller('dashboardCtrl', function ($scope, restService, $location, ngDialog) {
     var dash_timeStamp = new Date().getTime();
-    $scope.dashBoardData=hrDashData.dashBoardData;
+    $scope.dashBoardData = hrDashData.dashBoardData;
 
     // dashboard initialization
-    $scope.dashPage=function(){
-    var dashBoard = new Date();
-    $scope.previous = dashBoard.setDate(dashBoard.getDate() - 1);
-    var query = {
-        timeStamp: dash_timeStamp
+    $scope.dashPage = function () {
+        var dashBoard = new Date();
+        $scope.previous = dashBoard.setDate(dashBoard.getDate() - 1);
+        var query = {
+            timeStamp: dash_timeStamp
+        };
+        // restService call for Dashboard data..
+        var promise = restService.httpRequest('readDashboardData', query, "get");
+        promise.then(function (data) {
+            var dashData = data.data;
+            $scope.dashData = dashData.data;
+            console.log($scope.dashData);
+        });
     };
-    // restService call for Dashboard data..
-    var promise = restService.httpRequest('readDashboardData', query,"get");
-
-
-    promise.then(function (data) {
-        var dashData = data.data;
-        $scope.dashData = dashData;
-        console.log($scope.dashData);
-    });
-  };
     //splitting key by space
     $scope.split = function (key) {
         key = key.split(/(?=[A-Z])/).join(" ");
@@ -36,24 +34,62 @@ angular.module('mainApp').controller('dashboardCtrl', function ($scope,restServi
     };
 
     //Popup form appears on clicking add employee
-    $scope.values = ['Email Address','Start Date','Hiring City', 'Coding Test At', 'Coding Test City', 'Stipend Ammount', 'Program Assigned', 'Tech Stack', 'Referred By',  'Company Assigned'];
-    $scope.employee ={};
-    $scope.addEmployee = function () {
-        $scope.name = "pranali";
+    $scope.values = ['Email Address', 'Planned Start Date', 'Hiring City', 'Coding Test At', 'Coding Test City', 'Employee Stipend', 'Current Program Assignment', 'Fellowship Tech Assignment', 'Referred By', 'Current Contract Assignment'];
+    $scope.employee = {};
+    $scope.createEmployee = function () {
+        $scope.employee = {};
         ngDialog.open({
-            template : 'addEmployee',
-            className : 'ngdialog-theme-default',
-            scope : $scope,
+            template: 'addEmployee',
+            className: 'ngdialog-theme-default',
+            scope: $scope,
             overlay: true,
             width: '80%',
             showClose: true
         });
     };
-    $scope.giveEmployee = function(employee){
-        var temp = employee.replace(' ','');
-        return temp;
+    $scope.giveEmployee = function (employee) {
+        var temp = employee.replace(/ /g, '');
+        return temp.charAt(0).toLowerCase() + temp.slice(1);
     };
-    $scope.createEmployee = function () {
-        console.log($scope.employee);
-    }
+
+    $scope.addEmployeeDetails = function () {
+        ngDialog.close();
+        var msg = '';
+        $scope.color = 'blue';
+        restService.httpRequest('sendInviteLink', $scope.employee, 'post').then(function (data) {
+            console.log(data);
+            if (data.data.status) {
+                msg = data.data.message;
+                ngDialog.open({
+                    template: "<text style='color: #4cae4c;font-size: 16px; font-weight: 600'>" + msg + "</text>",
+                    className: 'ngdialog-theme-default',
+                    plain: true,
+                    overlay: true,
+                    width: '45%',
+                    showClose: false
+                });
+            }
+            else {
+                if (data.data.status === false)
+                    msg = data.data.message;
+                else
+                    msg = 'Cannot Submit Form! Please contact to Developers';
+                ngDialog.open({
+                    template: "<text style='color: red;font-size: 16px; font-weight: 600'>" + msg + "</text>",
+                    className: 'ngdialog-theme-default',
+                    plain: true,
+                    overlay: true,
+                    width: '45%',
+                    showClose: false
+                });
+            }
+
+        });
+        $scope.employee = {};
+    };
+    /*jQuery for showing datepicker*/
+    $('#sandbox-container input').datepicker({
+        autoclose: true
+    });
+
 });
